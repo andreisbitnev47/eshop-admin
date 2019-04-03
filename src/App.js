c07c0f8204131283936196a6babe86ec1288173d
Asd asd <asd@asd.asd>
2019-04-03 03:46:43 -0400

    add image resource

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Admin, Resource, EditGuesser, ListGuesser } from 'react-admin';
 import { UserList } from './users';
 import { ProductList, ProductEdit, ProductCreate } from './products';
+import { OrderList, OrderEdit} from './orders';
+import { ImageList,ImageCreate } from './image';
 import jsonServerProvider from 'ra-data-json-server';
 import authProvider from './authProvider';
 import dataProvider from './dataProvider';
@@ -9,7 +11,9 @@ import dataProvider from './dataProvider';
 // const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource name="product" list={ProductList} edit={ProductEdit} create={ProductCreate}/>
+    <Resource name="product" list={ProductList} edit={ProductEdit} create={ProductCreate} />
+    <Resource name="order" list={OrderList} />
+    <Resource name="image" list={ImageList} create={ImageCreate}/>
   </Admin>
 );
 
